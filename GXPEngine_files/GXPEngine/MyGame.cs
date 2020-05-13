5dbb0c44c325dbfff6ffcefd14f3489e544efc82
jo2n64 <valio31032@gmail.com>
2020-05-13 22:27:32 +0300

    Added an easter egg (just dont look for it if you dont like eerie memes)

--- GXPEngine_files/GXPEngine/MyGame.cs
+++ GXPEngine_files/GXPEngine/MyGame.cs
@@ -4,26 +4,34 @@ using System.Drawing;
 
 public class MyGame : Game
 {
-    Button play, options, exit;
-    Sprite bg;
+    Button play, options, exit, easterEgg;
+    Sprite bg, changedaworld;
     Level level;
     Options option;
     public SoundChannel musicChannel;
-    public Sound music;
+    public Sound music, change;
     public bool isPlaying;
-    bool hasStarted;
+    int timer;
+    bool hasStarted, inEasterEgg;
     public MyGame() : base(1600, 900, false)        // Create a window that's 800x600 and NOT fullscreen
     {
+        timer = 8216;
         isPlaying = false;
+        inEasterEgg = false;
         bg = new Sprite("mainmenu.png");
+        changedaworld = new Sprite("changedaworld.png");
+        changedaworld.SetScaleXY(3.3f, 1.9f);
+        change = new Sound("myfinalmessage.mp3");
         bg.SetScaleXY(1.05f);
         play = new Button(new Vec2(width / 2 - 130, height / 2-10), 250, 100, "Welcome");
         options = new Button(new Vec2(width / 2 - 100, height / 2 + 100), 210, 90, "Options");
         exit = new Button(new Vec2(width / 2 - 60, height / 2 + 200), 160, 60, "Exit");
         music = new Sound("freshTank.mp3");
+        easterEgg = new Button(new Vec2(width - 100, 0), 100, 100, "dont click if you dont\nwanna have nightmares");
         option = new Options();
 
         AddChild(bg);
+        
         //AddChild(play);
         //AddChild(options);
         //AddChild(exit);
@@ -47,6 +55,13 @@ public class MyGame : Game
             isPlaying = true;
         }
 
+        if(CheckMouseInRectClick(changedaworld) && !hasStarted && !inEasterEgg)
+        {
+            musicChannel = change.Play();
+            AddChild(changedaworld);
+            inEasterEgg = true;
+        }
+
         if (musicChannel != null)
         {
             if (!option.isMusicPlaying)
@@ -56,6 +71,19 @@ public class MyGame : Game
             if (option.isMusicPlaying)
             {
                 musicChannel.Volume = 1f;
+            }
+        }
+
+        if(inEasterEgg)
+        {
+            timer -= Time.deltaTime;
+            if(timer <= 0)
+            {
+                changedaworld.alpha -= 0.005f;
+            }
+            if(changedaworld.alpha <= 0f)
+            {
+                RemoveChild(changedaworld);
             }
         }
 
