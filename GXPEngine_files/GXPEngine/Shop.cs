9cf18f8eb99139983d2ac05e92b79ffe92b1b417
Ana-Maria Ilea <62722821+Ana-MariaIlea@users.noreply.github.com>
2020-05-14 22:54:47 +0200

    shop clean code

--- GXPEngine_files/GXPEngine/Shop.cs
+++ GXPEngine_files/GXPEngine/Shop.cs
@@ -2,160 +2,209 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using GXPEngine;
 
-namespace GXPEngine
+class Shop : GameObject
 {
-    class Shop:GameObject
+    private List<Fish> fishList;
+    private Level _level;
+    private Sound buyFish;
+    private Sound notEnpughMoneyToBuyFish;
+    private Sprite close;
+    private Sprite notEnoughMoney;
+    private Inventory inv;
+    private Options _option;
+    //------------------------------------------------------------------------
+    //                          Counstructor
+    //------------------------------------------------------------------------
+    public Shop(List<Fish> fishListOfTank, Level level, Inventory inventory, Options option)
     {
-        List<Fish> fishList;
-        Level _level;
-        Sound buyFish;
-        Sound notEnpughMoneyToBuyFish;
-        Sprite close;
-        Sprite notEnoughMoney;
-        Inventory inv;
-        Options _option;
-        public Shop(List<Fish> fishListOfTank,Level level, Inventory inventory,Options option)
+        _option = option;
+        close = new Sprite("close_button.png");
+        notEnoughMoney = new Sprite("no_money.png");
+        notEnoughMoney.width /= 10;
+        notEnoughMoney.height /= 10;
+        close.width /= 5;
+        close.height /= 5;
+        _level = level;
+        fishList = fishListOfTank;
+        buyFish = new Sound("buying_fish_sound.mp3", false, true);
+        notEnpughMoneyToBuyFish = new Sound("no_money_sound.wav", false, true);
+        makeShop();
+        inv = inventory;
+        AddChild(close);
+    }
+    //------------------------------------------------------------------------
+    //                          makeShop
+    //------------------------------------------------------------------------
+    private void makeShop()
+    {
+        int i = 1;
+        int j = 1;
+        makeBackground();
+        foreach (Fish fish in fishList)
         {
-            _option = option;
-            close = new Sprite("close_button.png");
-            notEnoughMoney = new Sprite("no_money.png");
-            notEnoughMoney.width /= 10;
-            notEnoughMoney.height /= 10;
-            close.width /= 5;
-            close.height /= 5;
-            _level = level;
-            fishList = fishListOfTank;
-            buyFish = new Sound("buying_fish_sound.mp3", false, true);
-            notEnpughMoneyToBuyFish = new Sound("no_money_sound.wav", false, true);
-            makeShop();
-            inv = inventory;
-            AddChild(close);
-        }
-        void makeShop()
-        {
-            int i = 1;
-            int j = 1;
-            makeBackground();
-            foreach (Fish fish in fishList)
+            if (fish.GetIsUnlocked() == false)
             {
-                if (fish.GetIsUnlocked() == false)
+                makeIconsAndButtons(i, j, fish);
+                i++;
+                if (i >= 4)
                 {
-                    AddChild(fish.buyToUnlock);
-                    fish.buyToUnlock.x = i * game.width / 4 - fish.buyToUnlock.width / 2;
-                    fish.buyToUnlock.y = j * game.height / 3+105;
-
-                    AddChild(fish.fishNameAndPrice);
-                    fish.fishNameAndPrice.x = i * game.width / 4 - fish.fishNameAndPrice.width / 2;
-                    fish.fishNameAndPrice.y = j * game.height / 3 + 10;
-
-                    makeIconsForFish(i, j, fish);
-
-                    i++;
-                    if (i >= 4)
-                    {
-                        j++;
-                        i = 1;
-                    }
+                    j++;
+                    i = 1;
                 }
             }
         }
-
-        private void makeIconsForFish(int i, int j, Fish fish)
+    }
+    //------------------------------------------------------------------------
+    //                          makeIconsAndButtons
+    //------------------------------------------------------------------------
+    private void makeIconsAndButtons(int i, int j, Fish fish)
+    {
+        makeBuyButton(i, j, fish);
+        makeFishNameAndPriceIcon(i, j, fish);
+        makeIconsForFish(i, j, fish);
+    }
+    //------------------------------------------------------------------------
+    //                          makeBuyButton
+    //------------------------------------------------------------------------
+    private void makeBuyButton(int i, int j, Fish fish)
+    {
+        AddChild(fish.buyToUnlock);
+        fish.buyToUnlock.x = i * game.width / 4 - fish.buyToUnlock.width / 2;
+        fish.buyToUnlock.y = j * game.height / 3 + 105;
+    }
+    //------------------------------------------------------------------------
+    //                          makeFishNameAndPriceIcon
+    //------------------------------------------------------------------------
+    private void makeFishNameAndPriceIcon(int i, int j, Fish fish)
+    {
+        AddChild(fish.fishNameAndPrice);
+        fish.fishNameAndPrice.x = i * game.width / 4 - fish.fishNameAndPrice.width / 2;
+        fish.fishNameAndPrice.y = j * game.height / 3 + 10;
+    }
+    //------------------------------------------------------------------------
+    //                          makeIconsForFish
+    //------------------------------------------------------------------------
+    private void makeIconsForFish(int i, int j, Fish fish)
+    {
+        Sprite fishIcon = new Sprite(fish.GetFishName() + "-icon.png");
+        AddChild(fishIcon);
+        fishIcon.SetOrigin(fishIcon.width / 2, fishIcon.height / 2);
+        fishIcon.width /= 8;
+        fishIcon.height /= 8;
+        fishIcon.x = i * game.width / 4;
+        fishIcon.y = j * game.height / 3 - fishIcon.height / 2 + 20;
+    }
+    //------------------------------------------------------------------------
+    //                          makeBackground
+    //------------------------------------------------------------------------
+    private void makeBackground()
+    {
+        Sprite backgroundShop = new Sprite("shop_shelf.png");
+        AddChild(backgroundShop);
+        backgroundShop.width = game.width - game.width / 5;
+        backgroundShop.height = game.height - game.height / 5;
+        backgroundShop.x = game.width / 10;
+        backgroundShop.y = game.height / 10 - 20;
+        close.x = backgroundShop.width + game.width / 10 - close.width - 20;
+        close.y = game.height / 10;
+    }
+    //------------------------------------------------------------------------
+    //                          Update
+    //------------------------------------------------------------------------
+    void Update()
+    {
+        if (visible == true)
         {
-            Sprite fishIcon = new Sprite(fish.GetFishName() + "-icon.png");
-            AddChild(fishIcon);
-            fishIcon.SetOrigin(fishIcon.width / 2, fishIcon.height / 2);
-            fishIcon.width /= 8;
-            fishIcon.height /= 8;
-            fishIcon.x = i * game.width / 4;
-            fishIcon.y = j * game.height / 3 - fishIcon.height / 2+20;
+            handleBuyingFish();
+            closeShop();
         }
-
-        private void makeBackground()
+        else
         {
-            Sprite backgroundShop = new Sprite("shop_shelf.png");
-            AddChild(backgroundShop);
-            backgroundShop.width = game.width - game.width / 5;
-            backgroundShop.height = game.height - game.height / 5;
-            backgroundShop.x = game.width / 10;
-            backgroundShop.y = game.height / 10-20;
-            close.x = backgroundShop.width+ game.width / 10-close.width-20;
-            close.y = game.height / 10;
-        }
-
-        void Update()
-        {
-            if (visible == true)
+            if (HasChild(notEnoughMoney))
             {
-                foreach (Fish fish in fishList)
-                {
-
-                    if (MyGame.CheckMouseInRectClick(fish.buyToUnlock))
-                    {
-
-                        if (_level.currencySystem.money >= fish.GetFishPrice())
-                        {
-
-                            if (fish.GetIsUnlocked() == false)
-                            {
-                                if (_option.isSoundPlaying)
-                                {
-                                    buyFish.Play();
-                                }
-                                _level.currencySystem.RemoveMoney(fish.GetFishPrice());
-                                fish.Unlock();
-                                _level.journal.AddFish(fish);
-                                AddChild(fish.soldOut);
-                                fish.soldOut.x = fish.buyToUnlock.x;
-                                fish.soldOut.y = fish.buyToUnlock.y-10;
-                                RemoveChild(fish.buyToUnlock);
-                                RemoveChild(fish.fishNameAndPrice);
-                                if(_level.tutorial.count == 6)
-                                {
-                                    _level.tutorial.count = 7;
-                                }
-                            }
-
-                        }
-                        else
-                        {
-                            if (fish.GetIsUnlocked() == false)
-                            {
-                                if (_option.isSoundPlaying)
-                                {
-                                    notEnpughMoneyToBuyFish.Play();
-                                }
-                                AddChild(notEnoughMoney);
-                                notEnoughMoney.x = fish.buyToUnlock.x;
-                                notEnoughMoney.y = fish.buyToUnlock.y;
-                            }
-
-
-                        }
-                    }
-
-                }
-
-                if (MyGame.CheckMouseInRectClick(close))
-                {
-                    visible = false;
-                    inv.DeselectShop();
-                    if(_level.tutorial.count == 7)
-                    {
-                        _level.tutorial.count = 8;
-                    }
-
-                }
+                RemoveChild(notEnoughMoney);
             }
-            else
+        }
+    }
+    //------------------------------------------------------------------------
+    //                          handleBuyingFish
+    //------------------------------------------------------------------------
+    private void handleBuyingFish()
+    {
+        foreach (Fish fish in fishList)
+        {
+            if (MyGame.CheckMouseInRectClick(fish.buyToUnlock))
             {
-                if (HasChild(notEnoughMoney))
+
+                if (_level.currencySystem.money >= fish.GetFishPrice())
                 {
-                    RemoveChild(notEnoughMoney);
+                    fishIsBought(fish);
+                }
+                else
+                {
+                    fishIsNotBought(fish);
                 }
             }
         }
+    }
+    //------------------------------------------------------------------------
+    //                          closeShop
+    //------------------------------------------------------------------------
+    private void closeShop()
+    {
+        if (MyGame.CheckMouseInRectClick(close))
+        {
+            visible = false;
+            inv.DeselectShop();
+            if (_level.tutorial.count == 7)
+            {
+                _level.tutorial.count = 8;
+            }
 
+        }
+    }
+    //------------------------------------------------------------------------
+    //                          fishIsNotBought
+    //------------------------------------------------------------------------
+    private void fishIsNotBought(Fish fish)
+    {
+        if (fish.GetIsUnlocked() == false)
+        {
+            if (_option.isSoundPlaying)
+            {
+                notEnpughMoneyToBuyFish.Play();
+            }
+            AddChild(notEnoughMoney);
+            notEnoughMoney.x = fish.buyToUnlock.x;
+            notEnoughMoney.y = fish.buyToUnlock.y;
+        }
+    }
+    //------------------------------------------------------------------------
+    //                          fishIsBought
+    //------------------------------------------------------------------------
+    private void fishIsBought(Fish fish)
+    {
+        if (fish.GetIsUnlocked() == false)
+        {
+            if (_option.isSoundPlaying)
+            {
+                buyFish.Play();
+            }
+            _level.currencySystem.RemoveMoney(fish.GetFishPrice());
+            fish.Unlock();
+            _level.journal.AddFish(fish);
+            AddChild(fish.soldOut);
+            fish.soldOut.x = fish.buyToUnlock.x;
+            fish.soldOut.y = fish.buyToUnlock.y - 10;
+            RemoveChild(fish.buyToUnlock);
+            RemoveChild(fish.fishNameAndPrice);
+            if (_level.tutorial.count == 6)
+            {
+                _level.tutorial.count = 7;
+            }
+        }
     }
 }
+
